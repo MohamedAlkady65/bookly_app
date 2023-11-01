29cb7abd1403b6a6af28209c6cb2592260c35285
MohamedAlkady <mohamedalkady65@gmail.com>
2023-11-01 21:46:28 +0200

    add play icon to featured item

--- lib/Features/home/presentaion/views/widgets/featured_item.dart
+++ lib/Features/home/presentaion/views/widgets/featured_item.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class FeaturedItem extends StatelessWidget {
@@ -7,19 +9,45 @@ class FeaturedItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2 / 3,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          // image: const DecorationImage(
-          //   fit: BoxFit.fill,
-          //   image: AssetImage("assets/images/test_item.png"),
-          // ),
-        ),
-        child: Image.asset(
-          "assets/images/test_item.png",
-          fit: BoxFit.fill,
-        ),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              // image: const DecorationImage(
+              //   fit: BoxFit.fill,
+              //   image: AssetImage("assets/images/test_item.png"),
+              // ),
+            ),
+            child: Image.asset(
+              "assets/images/test_item.png",
+              fit: BoxFit.fill,
+            ),
+          ),
+          playIcon(),
+        ],
       ),
     );
+  }
+
+  Positioned playIcon() {
+    return Positioned(
+          right: 15,
+          bottom: 10,
+          child: SizedBox(
+            width: 45,
+            height: 45,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(99999),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: const Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        );
   }
 }
