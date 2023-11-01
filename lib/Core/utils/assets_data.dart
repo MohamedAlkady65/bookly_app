40ce9c8d7c2a4ad4f263ffddffa80a96e0eb4bc8
MohamedAlkady <mohamedalkady65@gmail.com>
2023-11-01 16:57:00 +0200

    splash screen

---
+++ lib/Core/utils/assets_data.dart
@@ -0,0 +1,4 @@
+class AssetsData {
+  static const logo = "assets/images/logo.svg";
+  static const loading = "assets/images/loading.json";
+}
---
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/Core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SvgPicture.asset(
+          AssetsData.logo,
+          width: 250,
+        ),
+      ],
+    );
+  }
+}
